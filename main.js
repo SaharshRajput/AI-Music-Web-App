50d8c06123b5d31c20c5ffad1fd9caf03329831d
SaharshRajput <73647463+SaharshRajput@users.noreply.github.com>
2021-06-05 13:38:39 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
 song1="";
 song2="";
+leftWristX="";
+leftWristY="";
+rightWristX="";
+rightWristY="";
+
+
+
+
+
+
+
+
+
 function preload()
 {
     song1=loadSound("Dil Bechara (1).mp3");
@@ -8,10 +21,31 @@ function preload()
 function setup()
 {
     canvas=createCanvas(600,450);
-    canvas.position(380,220);
+    canvas.center();
 
     video=createCapture(VIDEO);
     video.hide();
+
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on("pose",gotPoses);
+}
+function modelLoaded()
+{
+    console.log("Model is loaded");
+}
+function gotPoses(results)
+{
+    if(results.length>0)
+    {
+        console.log(results);
+        leftWristX=results[0].pose.leftWrist.x;
+        leftWristY=results[0].pose.leftWrist.y;
+        console.log("Left Wrist X= "+leftWristX+" Left Wrist Y ="+leftWristY);
+
+        rightWristX=results[0].pose.rightWrist.x;
+        rightWristY=results[0].pose.rightWrist.y;
+        console.log(" Right Wrist X= "+rightWristX+" Right Wrist Y="+rightWristY);
+    }
 }
 function draw()
 {
